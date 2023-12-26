eb659128f1faf77b5e78eb14ea6a759f70b87da2
Faisal Misbah <132295428+FaisalMisbah23@users.noreply.github.com>
2023-12-26 16:04:55 +0500

    Final

--- script.js
+++ script.js
@@ -230,3 +230,14 @@ function typeWriter() {
   i++;
   setTimeout(typeWriter, speed);
 }
+
+
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
