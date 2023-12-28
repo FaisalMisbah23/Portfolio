f51d2bc55360534a3fcf21f7edcd4fd5f0e387e8
Faisal Misbah <132295428+FaisalMisbah23@users.noreply.github.com>
2023-12-28 23:36:53 +0500

    Final

--- script.js
+++ script.js
@@ -241,6 +241,57 @@ function closeNav1() {
   document.getElementById("mySidenav1").style.width = "0";
 }
 
+function openNav2() {
+  document.getElementById("mySidenav2").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav2() {
+  document.getElementById("mySidenav2").style.width = "0";
+}
+
+function openNav3() {
+  document.getElementById("mySidenav3").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav3() {
+  document.getElementById("mySidenav3").style.width = "0";
+}
+
+function openNav4() {
+  document.getElementById("mySidenav4").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav4() {
+  document.getElementById("mySidenav4").style.width = "0";
+}
+function openNav5() {
+  document.getElementById("mySidenav5").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav5() {
+  document.getElementById("mySidenav5").style.width = "0";
+}
+function openNav6() {
+  document.getElementById("mySidenav6").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav6() {
+  document.getElementById("mySidenav6").style.width = "0";
+}
+function openNav7() {
+  document.getElementById("mySidenav7").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav7() {
+  document.getElementById("mySidenav7").style.width = "0";
+}
+
 
 
 
